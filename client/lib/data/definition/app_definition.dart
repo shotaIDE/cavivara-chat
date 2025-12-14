e6158931370480da73155cd31b0e926ea2167653
Shota Ide <ide.shota.dev@gmail.com>
2025-12-14 09:19:51 +0900

    Update app_definition.dart

--- client/lib/data/definition/app_definition.dart
+++ client/lib/data/definition/app_definition.dart
@@ -1,1 +1,9 @@
 const appStoreId = '000000000';
+
+const revenueCatProjectGoogleApiKey = String.fromEnvironment(
+  'REVENUE_CAT_PROJECT_GOOGLE_API_KEY',
+);
+
+const revenueCatProjectAppleApiKey = String.fromEnvironment(
+  'REVENUE_CAT_PROJECT_APPLE_API_KEY',
+);
