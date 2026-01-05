daf58b937aaff79ecf728dc0ef4a56534ebb128a
Shota Ide <ide.shota.dev@gmail.com>
2026-01-06 00:25:24 +0900

    Revert "refactor: デバッグ画面のPresenterを簡素化"
    This reverts commit 510b9bb7628b2ce9ac3482f97a759d3c90176650.

--- client/lib/ui/feature/settings/debug_presenter.dart
+++ client/lib/ui/feature/settings/debug_presenter.dart
@@ -1,13 +1,18 @@
+import 'package:house_worker/data/model/user_profile.dart';
 import 'package:house_worker/data/service/auth_service.dart';
 import 'package:house_worker/ui/root_presenter.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'debug_presenter.g.dart';
 
+/// デバッグ画面のPresenter
 @riverpod
 class DebugPresenter extends _$DebugPresenter {
   @override
-  void build(Ref ref) {}
+  Future<UserProfile?> build() async {
+    final userProfile = await ref.watch(currentUserProfileProvider.future);
+    return userProfile;
+  }
 
   /// ログアウト処理
   Future<void> logout() async {
